d173417e2140962fb8f9a8184eb24399bd09ea2b
Ivadriil <petrivi51@gmail.com>
2026-03-14 17:10:09 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-// write code here
+const list = document.querySelector('ul');
+
+function getSalaryNumber(salary) {
+  return Number(salary.replace(/[$,]/g, ''));
+}
+
+function sortList(listElement) {
+  const items = Array.from(listElement.children);
+
+  items.sort((a, b) => {
+    const salaryA = getSalaryNumber(a.dataset.salary);
+    const salaryB = getSalaryNumber(b.dataset.salary);
+
+    return salaryB - salaryA;
+  });
+  items.forEach((item) => listElement.appendChild(item));
+}
+
+function getEmployees(listElement) {
+  const people = Array.from(listElement.children);
+
+  people.forEach((person) => {
+    const name1 = person.textContent.trim();
+    const position = person.dataset.position;
+    const salary = person.dataset.salary;
+    const age = person.dataset.age;
+
+    return `Name: ${name1}, Position: ${position}, Salary: ${salary}, Age: ${age}`;
+  });
+}
+
+sortList(list);
+getEmployees(list);
